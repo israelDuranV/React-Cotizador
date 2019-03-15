8e25b94c30259aa8d90322b3cb697c306815678f
israelDuranV <israel_duran@outlook.com>
2019-03-14 22:24:05 -0600

    Estoy comenzando con los componentes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+<h1>holaaa ap</h1>
+      </div>
+    );
+  }
+}
+export default App
